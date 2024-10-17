791b83dcd40f95f9d93f05ed736f4d884835ed83
Vitaliy-Avdeev <avd.vit78@gmail.com>
2024-10-17 15:23:58 +0300

    Project completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,73 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        var taskOne = ("Задача №1, №2");
+        System.out.println(taskOne);
+
+        int clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+        int age = 2014;
+        if (age < 2015) if (clientOS == 0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+
+        var taskThree = ("Задача №3");
+        System.out.println(taskThree);
+        int year = 1921;
+        if (year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else if (year % 4 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+
+        var taskFour = ("Задача №4");
+        System.out.println(taskFour);
+        int deliveryDistance = 95;
+        int twenty = 1;
+        int twentySixty = 2;
+        int sixtyOneHundred = 3;
+        boolean moreThanHundred = deliveryDistance > 100;
+        if (deliveryDistance <= 20) {
+            System.out.println("Потребуется " + twenty + " день доставки");
+        }
+        if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            System.out.println("Потребуется " + twentySixty + " дня доставки");
+        }
+        if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            System.out.println("Потребуется " + sixtyOneHundred + " дня доставки");
+        }
+        if (moreThanHundred) {
+            System.out.println("Доставка не осуществляется");
+        }
+        var taskFive = ("Задача №5");
+        System.out.println(taskFive);
+        int monthNumber = 12;
+        String monthName = switch (monthNumber) {
+            case 1 -> "Январь - зима";
+            case 2 -> "Февраль - зима";
+            case 3 -> "Март - весна";
+            case 4 -> "Апрель - весна";
+            case 5 -> "Май - весна";
+            case 6 -> "Июнь - лето";
+            case 7 -> "Июль - лето";
+            case 8 -> "Август - лето";
+            case 9 -> "Сентябрь - осень";
+            case 10 -> "Октябрь - осень";
+            case 11 -> "Ноябрь - осень";
+            case 12 -> "Декабрь - зима";
+            default -> "";
+        };
+        System.out.println(monthName);
+
     }
 }
